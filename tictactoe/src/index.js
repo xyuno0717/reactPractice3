27cb41d5d15910fef74d96aa95c52c8eea8ea043
xyumu56 <xyumu56@gmail.com>
2018-08-15 17:38:34 +0900

    Lifting State Up

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
 	}
 	render() {
 		return (
-			<button className="square" onClick={()=>this.setState({value:'X'})}>
+			<button className="square" onClick={()=>this.props.onClick()}>
 			{this.state.value}
 			</button>
 		);
@@ -19,8 +19,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+	constructor(){
+		super();
+		this.state={
+			squares: Array(9).fill(null),
+		};
+	}
+	handleClick(i){
+		const squares = this.state.squares.slice();
+		squares[i]='X';
+		this.setState({squares:squares});
+	}
 	renderSquare(i) {
-		return <Square value={i}/>;
+		return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>;
 	}
 	render() {
 		const status = 'Next player: X';
